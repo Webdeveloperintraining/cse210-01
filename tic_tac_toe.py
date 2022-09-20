7fe78b76e67e896a71d655eca8f3610b74425f4d
Webdeveloperintraining <sil22006@byui.edu>
2022-09-20 11:29:32 -0700

    game finished

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,46 +1,40 @@
-# #Author:Irvin Silva -- Subject CSE 210 -- Instructor name: Brother Duane Richards
-
+#Author:Irvin Silva -- Subject CSE 210 -- Instructor name: Brother Duane Richards
 def main():
     round=0
+    player=0
     print("Welcome to Tic-Tac-Toe")
     game="1|2|3\n-+-+-\n4|5|6\n-+-+-\n7|8|9"
     print(game)
-
-    while round !=5:
-        move1=str(input('Please select a number (1-9) '))
-        while move1 not in game:       
-            print("Sorry, that's an invalid input, please try again")
-            move1=str(input('Please select a number (1-9) '))
-        else:
-            game= game.replace(move1,"X")
+    while round<=8:
+        player=players(round)
+        game=play(player,game)
         print(game)
-
-        move2=str(input('Please select a number (1-9) '))        
-        while move2 not in game:   
-            print("Sorry, that's an invalid input, please try again")
-            move2=str(input('Please select a number (1-9) ')) 
-        else:
-            game= game.replace(move2,"O")
-        print(game)
-        if win_condition(game):
-            break
         round+=1
-
-
-    
+        print()
+        if win_condition(game,player):
+            #print(f"Thanks for playing, Player {player} wins")
+            break
+    #print(f"Thanks for playing, Player {player} wins")
     print('Game Over')
+    
 
-def win_condition(game):
-    win_conditions1=["X|X|X\n-+-+-\n4|5|6\n-+-+-\n7|8|9","1|2|3\n-+-+-\nX|X|X\n-+-+-\n7|8|9","1|2|3\n-+-+-\n4|5|6\n-+-+-\nX|X|X","X|2|3\n-+-+-\n4|X|6\n-+-+-\n7|8|X","1|2|X\n-+-+-\n4|X|6\n-+-+-\nX|8|9","X|2|3\n-+-+-\nX|5|6\n-+-+-\nX|8|9","1|X|3\n-+-+-\n4|X|6\n-+-+-\n7|X|9","1|2|X\n-+-+-\n4|5|X\n-+-+-\n7|8|X"]
-    win_conditions2=["O|O|O\n-+-+-\n4|5|6\n-+-+-\n7|8|9","1|2|3\n-+-+-\nO|O|O\n-+-+-\n7|8|9","1|2|3\n-+-+-\n4|5|6\n-+-+-\nO|O|O","O|2|3\n-+-+-\n4|O|6\n-+-+-\n7|8|O","1|2|O\n-+-+-\n4|O|6\n-+-+-\nO|8|9","O|2|3\n-+-+-\nO|5|6\n-+-+-\nO|8|9","1|O|3\n-+-+-\n4|O|6\n-+-+-\n7|O|9","1|2|O\n-+-+-\n4|5|O\n-+-+-\n7|8|O"]
-    if game in win_conditions1:
-        print("Thanks for playing, player 1 wins")
-    elif game in win_conditions2:
-        print("Thanks for playing, player 2 wins")
+def players(round):
+    if round in range(0,10,2):
+        player="X"
+    else:
+        player="O"
+    return player
+
+def play(player,game):
+    move=str(input(f'Player {player}, please select a number (1-9) '))
+    while move not in game:   
+        print("Sorry, that's an invalid input, please try again")
+        move=str(input('Please select a number (1-9) ')) 
+    game= game.replace(move,player)
+    return game
+
+def win_condition(game,player):
+    if player == game[0]== game[2] == game[4] or game[12]== game[14] == game[16] or game[24]== game[26] == game[28] or game[0]== game[14] == game[28] or game[4]== game[14] == game[24] or game[0]== game[12] == game[24] or game[2]== game[14] == game[4] or game[0]== game[16] == game[28]:
+        print('Game over')        
 
 main()
-
-
-
-
-

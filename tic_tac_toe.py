8c64102af9d237089e0fb8dd6aaf0a90253a57d1
Webdeveloperintraining <sil22006@byui.edu>
2022-09-19 21:59:04 -0700

    Tic-Tac-Toe second approach to make the game

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -2,28 +2,36 @@
 
 def main():
     round=0
-    win_conditions=["X|X|X\n-+-+-\n4|5|6\n-+-+-\n7|8|9","1|2|3\n-+-+-\nX|X|X\n-+-+-\n7|8|9","1|2|3\n-+-+-\n4|5|6\n-+-+-\nX|X|X","X|2|3\n-+-+-\n4|X|6\n-+-+-\n7|8|X","1|2|X\n-+-+-\n4|X|6\n-+-+-\nX|8|9","X|2|3\n-+-+-\nX|5|6\n-+-+-\nX|8|9","1|X|3\n-+-+-\n4|X|6\n-+-+-\n7|X|9","1|2|X\n-+-+-\n4|5|X\n-+-+-\n7|8|X","O|O|O\n-+-+-\n4|5|6\n-+-+-\n7|8|9","1|2|3\n-+-+-\nO|O|O\n-+-+-\n7|8|9","1|2|3\n-+-+-\n4|5|6\n-+-+-\nO|O|O","O|2|3\n-+-+-\n4|O|6\n-+-+-\n7|8|O","1|2|O\n-+-+-\n4|O|6\n-+-+-\nO|8|9","O|2|3\n-+-+-\nO|5|6\n-+-+-\nO|8|9","1|O|3\n-+-+-\n4|O|6\n-+-+-\n7|O|9","1|2|O\n-+-+-\n4|5|O\n-+-+-\n7|8|O"]
     print("Welcome to Tic-Tac-Toe")
     game="1|2|3\n-+-+-\n4|5|6\n-+-+-\n7|8|9"
-    print(game)  
+    print(game)
+
     while round !=5:
-        move1=str(input('Please select a number (1-9) '))        
-        if move1 not in game:
+        move1=str(input('Please select a number (1-9) '))
+        while move1 not in game:       
             print("Sorry, that's an invalid input, please try again")
+            move1=str(input('Please select a number (1-9) '))
         else:
             game= game.replace(move1,"X")
         print(game)
+
         move2=str(input('Please select a number (1-9) '))        
-        if move2 not in game:
+        while move2 not in game:   
             print("Sorry, that's an invalid input, please try again")
+            move2=str(input('Please select a number (1-9) ')) 
         else:
             game= game.replace(move2,"O")
         print(game)
         round+=1
-        print (round)
-    print('game over')
-main()
 
+        if win_condition(game):
+            break
+    
+    print('Game Over')
+
+def win_condition(game):
+    win_conditions=["X|X|X\n-+-+-\n4|5|6\n-+-+-\n7|8|9","1|2|3\n-+-+-\nX|X|X\n-+-+-\n7|8|9","1|2|3\n-+-+-\n4|5|6\n-+-+-\nX|X|X","X|2|3\n-+-+-\n4|X|6\n-+-+-\n7|8|X","1|2|X\n-+-+-\n4|X|6\n-+-+-\nX|8|9","X|2|3\n-+-+-\nX|5|6\n-+-+-\nX|8|9","1|X|3\n-+-+-\n4|X|6\n-+-+-\n7|X|9","1|2|X\n-+-+-\n4|5|X\n-+-+-\n7|8|X","O|O|O\n-+-+-\n4|5|6\n-+-+-\n7|8|9","1|2|3\n-+-+-\nO|O|O\n-+-+-\n7|8|9","1|2|3\n-+-+-\n4|5|6\n-+-+-\nO|O|O","O|2|3\n-+-+-\n4|O|6\n-+-+-\n7|8|O","1|2|O\n-+-+-\n4|O|6\n-+-+-\nO|8|9","O|2|3\n-+-+-\nO|5|6\n-+-+-\nO|8|9","1|O|3\n-+-+-\n4|O|6\n-+-+-\n7|O|9","1|2|O\n-+-+-\n4|5|O\n-+-+-\n7|8|O"]
+main()
 
 
 
